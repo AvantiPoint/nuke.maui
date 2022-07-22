3a2b9424f431e425c3e7746d86a9161e9576f70a
Dan Siegel <me@dansiegel.net>
2022-07-21 21:15:41 -0700

    add debug

--- src/AvantiPoint.Nuke.Maui/Windows/WinUIAppSigning.cs
+++ src/AvantiPoint.Nuke.Maui/Windows/WinUIAppSigning.cs
@@ -1,10 +1,12 @@
-﻿using AvantiPoint.Nuke.Maui.Tools.DotNet;
+﻿using System.Text.Json;
+using AvantiPoint.Nuke.Maui.Tools.DotNet;
 using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.AzureSignTool;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.SignTool;
+using Nuke.Common.Utilities.Collections;
 using Serilog;
 using static Nuke.Common.Tools.AzureSignTool.AzureSignToolTasks;
 using static Nuke.Common.Tools.SignTool.SignToolTasks;
@@ -53,7 +55,14 @@ internal static class WinUIAppSigning
         Assert.True(codeSign.AzureKeyVault.StartsWith("https://"), "The Uri must start with the https protocol");
         Assert.True(Uri.TryCreate(codeSign.AzureKeyVault, UriKind.Absolute, out var uri), "The supplied Azure Key Vault is not a valid Uri.");
 
-        DotNetToolHelper.EnsureInstalled("AzureSignTool");
+        var json = File.ReadAllText(codeSign.RootDirectory / "build" / "obj" / "project.assets.json");
+        var assets = JsonSerializer.Deserialize<ProjectAssets>(json);
+        Assert.True(assets.Project.Frameworks.ContainsKey("net6.0"), "Expected to find a net6.0 target in the build project.assets.json");
+
+        var framework = assets.Project.Frameworks["net6.0"];
+        framework.DownloadDependencies
+            .ForEach(x => Log.Information($"Download Dependency: {x.Name} - {x.Version}"));
+        //DotNetToolHelper.EnsureInstalled("AzureSignTool");
 
         AzureSignTool(_ => _
                 .SetKeyVaultUrl(codeSign.AzureKeyVault)
@@ -70,4 +79,9 @@ internal static class WinUIAppSigning
 
         return true;
     }
+
+    record ProjectAssets(Project Project);
+    record Project(Dictionary<string, Framework> Frameworks);
+    record Framework(DownloadDependency[] DownloadDependencies);
+    record DownloadDependency(string Name, string Version);
 }
